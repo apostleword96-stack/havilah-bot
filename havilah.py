e4f8a33734491db4330439781935575bdf62b532
apostleword96-stack <apostleword96@gmail.com>
2026-08-08 13:14:44 +0100

    Create havilah.py

---
+++ havilah.py
@@ -0,0 +1,16 @@
+import telebot
+    import os
+
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    bot = telebot.TeleBot(BOT_TOKEN)
+
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        bot.reply_to(message, "Hello! Havilah is online 🔥 How can I help you?")
+
+    @bot.message_handler(func=lambda message: True)
+    def echo_all(message):
+        bot.reply_to(message, f"You said: {message.text}")
+
+    print("Bot is running...")
+    bot.infinity_polling()
